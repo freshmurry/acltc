ad94f4ad147e4d1a74b578c58ee5d3649d1600a0
FreshMurry <lawrencemurry@yahoo.com>
2016-07-13 05:14:52 +0000

    first commit

---
+++ gets.rb
@@ -0,0 +1,8 @@
+puts "Enter your first name"
+x = gets.chomp
+puts "Enter your middle name"
+y = gets.chomp
+puts "Enter your last name"
+z = gets.chomp 
+puts "Your full name is:"
+puts x + " "  + y + " " + z
